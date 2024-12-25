da9f67ce7bd1ddacf0eca11f9233963a7acedb8b
SahilAnower <sahilanower2@gmail.com>
2024-12-25 15:51:06 +0530

    Made the changes to include the eureka server in the application

---
+++ eurekaserver/src/test/java/com/sahil/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahil.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
