338d5fcb4142527e32349aa585fffe165c8b95ef
SahilAnower <sahilanower2@gmail.com>
2024-12-30 17:43:56 +0530

    Added secured api with auth code flow alongside previous client credentials flow

--- gatewayserver/src/main/java/com/sahil/gatewayserver/securityconfig/SecurityConfig.java
+++ gatewayserver/src/main/java/com/sahil/gatewayserver/securityconfig/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -26,6 +27,7 @@ public class SecurityConfig {
                 .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
                         .jwt(jwtSpec -> jwtSpec.jwtAuthenticationConverter(grantedAuthoritiesExtractor())));
         serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable);
+        serverHttpSecurity.cors(Customizer.withDefaults());
         return serverHttpSecurity.build();
     }
 
