05fe7ae841934c2baa0e0beafb2f27add936d3fc
SahilAnower <sahilanower2@gmail.com>
2024-12-30 15:37:26 +0530

    Added transaction management in carts/products service for transactions to be managed by Spring

--- carts/src/main/java/com/sahil/carts/CartsApplication.java
+++ carts/src/main/java/com/sahil/carts/CartsApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @EnableConfigurationProperties(value = {CartContactInfoDto.class})
 @EnableFeignClients
+@EnableTransactionManagement
 public class CartsApplication {
 
 	public static void main(String[] args) {
--- products/src/main/java/com/sahil/products/ProductsApplication.java
+++ products/src/main/java/com/sahil/products/ProductsApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @EnableConfigurationProperties(value = {ProductsContactInfoDto.class})
+@EnableTransactionManagement
 public class ProductsApplication {
 
 	public static void main(String[] args) {
